57708dccad382f6c2d7f533771010e729680f8ea
Noam's Laptop <noaml12@hotmail.com>
2020-04-24 17:25:03 +0300

    added booster-pool-deck logic

--- src/Components/Main/index.js
+++ src/Components/Main/index.js
@@ -1,76 +1,12 @@
 
 import React, { Component } from 'react'
 // import { Card } from '@material-ui/core'
-import { CardCol } from 'Components'
+import { CardCol, MtgObject } from 'Components'
 import View from './view'
-import ScryfallClient from 'scryfall-client'
 
 const fact = 0.7;
 const cardWidth = 265;
 const cardHeight = 370;
-const scryfall = new ScryfallClient()
-
-function getAllCardsFromSet(setId)
-{
-    function collectCards(list, allCards)
-    {
-        allCards = allCards ? allCards : [];
-        allCards.push.apply(allCards, list);
-
-        if (!list.has_more)
-            return allCards;
-
-        return list.next().then((newList) => (
-            collectCards(newList, allCards)
-        ));
-    }
-
-    return scryfall.get('cards/search', {
-        q: 'e:'+[setId]
-    }).then(list => (
-        collectCards(list)
-    ))
-}
-
-function sortCardsBy(cards, by)
-{
-    var res = {};
-
-    switch (by)
-    {
-        default: //default is sort by color
-        case 'color':
-            res = {
-                R: [],
-                G: [],
-                B: [],
-                U: [],
-                W: [],
-                multiColor: [],
-                colorless: [],
-                land: [],
-            };
-
-            cards.forEach(card =>
-            {
-                if (card.type_line.toLowerCase().includes('basic land')) //basic land
-                    res.land.push(card);
-                else if (card.colors.length === 0) //colorless
-                    res.colorless.push(card);
-                else if (card.colors.length > 1) //multiColor
-                    res.multiColor.push(card)
-                else //monoColor
-                    res[card.colors[0]].push(card);
-            });
-
-            Object.entries(res).forEach(
-                ([key, value]) => value.sort(
-                    (a,b) => (a.cmc - b.cmc)))
-            break;
-    }
-    
-    return res;
-}
 
 /**
  * inputs:
@@ -84,36 +20,77 @@ class Main extends Component
         this.state = {
             ...props,
             className: props.className,
-            cards: {},
-            setId: 'iko', //'iko' for now, should get it from a dropdown list later
+            mtgObj: new MtgObject(),
+            cols: [],
         };
     }
     
     componentDidMount()
     {
-        getAllCardsFromSet(this.state.setId).then(fullList => {
-            this.setState({
-                cards: sortCardsBy(fullList.splice(0, 85), 'color'), //TODO change this
-                // cards: sortCardsBy(fullList, 'color'),
-            });
+        // getAllCardsFromSet(this.state.setId).then(fullList => {
+        //     this.setState({
+        //         cards: sortCardsBy(fullList.splice(0, 85), 'color'), //TODO change this
+        //         // cards: sortCardsBy(fullList, 'color'),
+        //     });
+        // });
+
+        this.state.mtgObj.generateSet('iko', 6, this);  //TODO add option to choose 'setId'
+                                                        //TODO add option to choose 'numOfBoosters'
+    }
+
+    handlePoolClick(e, i, colId)
+    {
+        this.setState( (currState, currProps) => {
+            let mtgObj = currState.mtgObj;
+            let cols = currState.cols;
+            
+            let clickedCard = cols[colId].splice(i, 1)[0]; //remove the card from the cols
+            mtgObj.removeCardFromPool(clickedCard); //remove the card from the pool itself
+            mtgObj.deck.push(clickedCard); //add the card to the deck
+
+            mtgObj.deck = MtgObject.sortCol(mtgObj.deck); //sort the deck by 'color->cmc'
+
+            return {
+                mtgObj: mtgObj,
+                cols: cols,
+            }
+        });
+    }
+
+    handleDeckClick(e, i)
+    {
+        this.setState( (currState, currProps) => {
+            let mtgObj = currState.mtgObj;
+            
+            let clickedCard = mtgObj.deck.splice(i, 1)[0]; //remove the card from the deck
+            mtgObj.pool.push(clickedCard); //add the card to the pool
+
+            let cols = MtgObject.sortCardsToColumns(mtgObj.pool, 'color');
+
+            return {
+                mtgObj: mtgObj,
+                cols: cols,
+            }
         });
     }
 
     render()
-    {
+    {  
         return(
             // <View {...this.state} />
             <div className="container-main">
                 <span className="container-cards">
                 {
-                    Object.entries(this.state.cards).map( ([key, value]) =>
+                    Object.entries(this.state.cols).map( ([key, value]) =>
                     (
                         <CardCol
                             key = {key}
+                            colId = {key}
+                            handleClick = {this.handlePoolClick.bind(this)}
                             className = "cardContainer"
                             cards = {value}
                             style = {{
-                                flex: 'flex-shrink'
+                                flex: 'flex-shrink',
                             }}
                         />
                     ))
@@ -122,10 +99,17 @@ class Main extends Component
                 <span
                     className = "container-side"
                     style = {{
-                        width: cardWidth * fact * 1.2
+                        width: cardWidth * fact * 1.1,
                     }}
                     >
-                    asd
+                        <CardCol
+                            handleClick = {this.handleDeckClick.bind(this)}
+                            className = "cardContainer"
+                            cards = {this.state.mtgObj.deck}
+                            style = {{
+                                flex: 'flex-shrink'
+                            }}
+                        />
                 </span>
             </div>
         );
--- src/Components/index.js
+++ src/Components/index.js
@@ -1,11 +1,12 @@
 // import Dashboard from './Dashboard/Dashboard'
 import Main from './Main'
 import Layout from './Layout'
-import { CardCol, CardBox } from './CardComponents'
+import { CardCol, CardBox, MtgObject } from './CardComponents'
 
 export {
     Main,
     Layout,
     CardCol,
     CardBox,
+    MtgObject,
 }
