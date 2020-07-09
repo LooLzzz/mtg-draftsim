b59e6c8a051841a3dd7ec982ef85a3ccec29a9ca
Naam's Desktop <noaml12@hotmail.com>
2020-07-09 16:34:16 +0300

    .

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 //server.js
 const express = require('express');
 const favicon = require('express-favicon');
-const path = require('patnpmh');
+const path = require('path');
 const port = process.env.PORT || 8080;
 const app = express();
 app.use(favicon(__dirname + '/build/favicon.ico'));
