e7ba6153caea1d8bdd824074bbcd22b85f5f0bb8
Noam's Laptop <noaml12@hotmail.com>
2020-09-29 12:06:12 +0300

    fixed routing + added top tab-nav

--- src/Components/Layout/index.js
+++ src/Components/Layout/index.js
@@ -1,8 +1,9 @@
-import React, { useState } from 'react';
+import React, { Component } from 'react';
 import clsx from 'clsx';
-import { makeStyles, useTheme } from '@material-ui/core/styles';
-import { AppBar, Toolbar, Typography } from '@material-ui/core';
+import { withStyles } from '@material-ui/core/styles';
+import { AppBar, Button, Tab, Tabs, Toolbar, Typography } from '@material-ui/core';
 import getStyles from './styles'
+import { withRouter } from 'react-router';
 // import { List, Drawer, Divider, IconButton, ListItemIcon, ListItemText } from '@material-ui/core';
 // import ChevronRightIcon from '@material-ui/icons/ChevronRight';
 // import ChevronLeftIcon from '@material-ui/icons/ChevronLeft';
@@ -12,12 +13,46 @@ import getStyles from './styles'
 // import MenuIcon from '@material-ui/icons/Menu';
 // import { Link } from 'react-router-dom';
 
-const useStyles = makeStyles((theme) => getStyles(theme));
+const useStyles = (theme) => getStyles(theme);
 
-export default function Layout(props)
+function tabNameToIndex(name)
 {
-    const classes = useStyles();
-    const theme = useTheme();
+    switch (name) {
+        default: //defaults to Main
+            return 0;
+        
+        case 'Draftsim':
+            return 1;
+    
+        case 'Collection':
+            return 2;
+    }
+}
+
+class Layout extends Component
+{
+    constructor(props)
+    {
+        super(props);
+        this.state = {...props}
+
+        this.handleClick = (event, item) => {
+            this.setState({
+                currentTab: item,
+            })
+            this.props.history.push('/' + item.toLowerCase())
+        }
+    }
+
+    componentDidUpdate(oldProps)
+    {
+        if (oldProps.currentTab !== this.props.currentTab)
+            this.setState({
+                ...this.props
+            })
+    }
+
+    // const theme = useTheme();
     // const [open, setOpen] = useState(false);
 
     // const handleDrawerToggle = () =>
@@ -25,78 +60,42 @@ export default function Layout(props)
     //     setOpen(!open);
     // };
 
-    return (
-        <div className={classes.root}>
-            <AppBar
-                position="absolute"
-                className={clsx(
-                    {
-                        [classes.appBar]: true, //always
-                        // [classes.appBarShift]: open, //only when (open===true)
-                    }
-                )}
-            >
-                <Toolbar>
-                    {/* <IconButton
-                        color="inherit"
-                        aria-label="open drawer"
-                        onClick={handleDrawerToggle}
-                        edge="start"
-                        className={clsx(
-                            {
-                                [classes.menuButton]: true,
-                                [classes.hide]: open
-                            }
-                        )}
-                    >
-                        <MenuIcon />
-                    </IconButton> */}
-                    <Typography noWrap variant="h6">
-                        {props.text}
-                    </Typography>
-                </Toolbar>
-            </AppBar>
-            {/* <Drawer
-                className={classes.drawer}
-                variant="persistent"
-                anchor="left"
-                open={open}
-                classes={{ paper: classes.drawerPaper }}
-            >
-                <div className={classes.drawerHeader}>
-                    <IconButton onClick={handleDrawerToggle}>
-                        {theme.direction === 'ltr' ? <ChevronLeftIcon /> : <ChevronRightIcon />}
-                    </IconButton>
-                </div>
-                <Divider />
-                <List>
-                    {['Inbox', 'Starred', 'Send email', 'Drafts'].map((text, index) => (
-                        <ListItem button key={text}>
-                            <ListItemIcon>{index % 2 === 0 ? <InboxIcon /> : <MailIcon />}</ListItemIcon>
-                            <ListItemText primary={text} />
-                        </ListItem>
-                    ))}
-                </List>
-            </Drawer> */}
-            <main
-                className={clsx(
-                    {
-                        // [classes.contentShift]: open,
-                        [classes.content]: true,
-                    })}
-            >
-                <div className={classes.contentSpacer} /> {/*top spacer*/}
-                { props.children }
-                <div className={classes.contentSpacer} /> {/*bottom spacer*/}
-            </main>
-            <AppBar
-                position="fixed"
-                className={clsx({[classes.bottomAppBar]: true})}
-            >
-                <Toolbar variant="dense">
-                    
-                </Toolbar>
-            </AppBar>
-        </div>
-    );
+    render()
+    {
+        const {classes} = this.props;
+
+        return (
+            <div className={classes.root}>
+                <AppBar
+                    position = "absolute"
+                    className = {classes.appBar}
+                >
+                    <Tabs value={tabNameToIndex(this.state.currentTab)}>
+                        {['Main', 'Draftsim', 'Collection'].map((item, i) => (
+                            <Tab
+                                key = {i}
+                                label = {item}
+                                onClick = {(event) => this.handleClick(event, item)}
+                            />
+                        ))}
+                    </Tabs>
+                </AppBar>
+                <main className={classes.content} >
+                    <div className={classes.contentSpacer} /> {/*top spacer*/}
+                    { this.props.children }
+                    <div className={classes.contentSpacer} /> {/*bottom spacer*/}
+                </main>
+                <AppBar
+                    position = "fixed"
+                    className = {classes.bottomAppBar}
+                >
+                    <Toolbar variant="dense">
+                        {/* bottom bar */}
+                    </Toolbar>
+                </AppBar>
+            </div>
+        );
+    }
 }
+
+export default withRouter(withStyles(useStyles)(Layout))
