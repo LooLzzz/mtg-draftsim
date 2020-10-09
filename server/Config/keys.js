033eca65dd74f83b2ae06e3bf34175b7ff904e2e
Naam's Desktop <noaml12@hotmail.com>
2020-10-09 10:27:41 +0300

    added .env and github secrets

--- server/Config/keys.js
+++ server/Config/keys.js
@@ -3,8 +3,8 @@
  * @type {{mongoURI: String}}
  */
 const Keys = {
-    mongoURI: "process.env.MONGO_URI",
-    secret: "process.env.SECRET_KEY"
+    mongoURI: process.env.MONGO_URI,
+    secret: process.env.SECRET_KEY
 }
 
 module.exports = Keys
